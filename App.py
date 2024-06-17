0472747b4b27ee2c85c8dc00f44887be5241f056
InfiniteOracles <evanacamper@gmail.com>
2024-06-17 12:53:49 -0400

    Simple UI [no functionality]

---
+++ App.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('Main.html')
+
+# main driver function
+if __name__ == '__main__':
+    app.run(debug=True)
